a47dc5da6a75fda7b6998130161d7e3257b643e0
Sharo2131 <153314492+Sharo2131@users.noreply.github.com>
2025-03-09 18:09:24 +0300

    Update settings.py

--- web/settings.py
+++ web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-neg*4pnyc!sjv-j0&=4z!g+l2cd8j**5!^*9zhq!o3367j6pqz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-project-xtmj.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
